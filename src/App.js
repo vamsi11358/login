dd06d708513bbe305211390f985c5a8507798074
vamsi krishna <vamsi11358@gmail.com>
2024-07-20 21:41:07 +0530

    login

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Login from './component/component';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/component.js
@@ -0,0 +1,36 @@
+import {useState} from 'react';
+
+export default function Login(){
+    const [text, setText] = useState('');
+    const [password, setPassword]= useState('');
+    const [login, setLogin] = useState(false);
+    const handleSubmit = ()=>{
+        if(text === 'user' && password === 'password'){
+            setLogin(true)
+        }else{
+            setLogin(false);
+        }
+    }
+    return(
+        <>
+        <h1>Login Page</h1>
+        {!login && (
+        <form  onSubmit={handleSubmit}>
+            <h4>Invalid username or password</h4>
+        <label for='userName'>User Name:</label>
+        <input placeholder='User Name' name='userName' type='userName' required onChange={(e)=>setText(e.target.value)}/>
+        <br/>
+        <label for='userName'>Password:</label>
+        <input placeholder='Password' type='password' name='password'  required onChange={(e)=>setPassword(e.target.value)}/>
+        <br/>
+        <button type='submit'>Submit</button>
+        </form>
+)}
+{login && (
+    <h1>Welcome ,user!</h1>
+)
+
+}
+        </>
+    )
+}
